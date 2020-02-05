b263794c0d3b7d6b8e9f4fa238fc26a87745f654
Kevin Klein <kleinkevin.lu@gmail.com>
2020-02-05 13:25:53 +0100

    Push org files to github.

--- fish/functions/org-push.fish
+++ fish/functions/org-push.fish
@@ -1,4 +1,7 @@
 # Defined in - @ line 2
 function org-push
-	rclone copy --exclude-from ~/org/orgignore.txt ~/org/ dbrem:org/
+	git --git-dir=/home/kevin/org/.git --work-tree=/home/kevin/org add *.org
+    git --git-dir=/home/kevin/org/.git --work-tree=/home/kevin/org commit -m "Update."
+    git --git-dir=/home/kevin/org/.git --work-tree=/home/kevin/org push origin master
+    rclone copy --exclude-from ~/org/orgignore.txt ~/org/ dbrem:org/
 end
