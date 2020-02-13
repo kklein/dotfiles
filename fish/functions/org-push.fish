4b26a13927366b0580d11548fe227c5bfdde1900
Kevin Klein <kleinkevin.lu@gmail.com>
2020-02-13 20:24:25 +0100

    Fix git add by making wildcard trigger in destination path.

--- fish/functions/org-push.fish
+++ fish/functions/org-push.fish
@@ -1,6 +1,6 @@
 # Defined in - @ line 2
 function org-push
-	git --git-dir=/home/kevin/org/.git --work-tree=/home/kevin/org add *.org
+	git --git-dir=/home/kevin/org/.git --work-tree=/home/kevin/org add '*.org'
     git --git-dir=/home/kevin/org/.git --work-tree=/home/kevin/org commit -m "Update."
     git --git-dir=/home/kevin/org/.git --work-tree=/home/kevin/org push origin master
     rclone copy --exclude-from ~/org/orgignore.txt ~/org/ dbrem:org/
