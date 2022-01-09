4c11b6dfdea0f66a0783a4963b82413e111b3d37
kklein <kleinkevin.lu@gmail.com>
2022-01-09 09:56:01 +0100

    Replace emacs config.

--- emacs/init.el
+++ emacs/init.el
@@ -1,22 +1,69 @@
-(require 'clojure-mode)
-(autoload 'paredit-mode "paredit"
-  "Minor mode for pseudo-structurally editing Lisp code." t)
-
-;; (add-hook clojure-mode-hook (lambda () (paredit-mode +1)))
-
 (setq inhibit-startup-message t)
+(scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(scroll-bar-mode -1)
-(global-hl-line-mode t)
-(line-number-mode t)
-(column-number-mode t)
-(setq fill-column 100)
+(fset 'yes-or-no-p 'y-or-n-p)
+(global-auto-revert-mode t)
+(global-display-line-numbers-mode)
 
+;; Dependency management
+(require 'package)
+
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+(defvar my-packages '(auto-complete
+		      elpy
+		      flycheck
+                      json-mode
+                      markdown-mode
+                      visual-fill-column
+                      yaml-mode
+                      ))
+
+(dolist (p my-packages)
+  (unless (package-installed-p p)
+    (package-refresh-contents)
+    (package-install p))
+  (add-to-list 'package-selected-packages p))
+
+(ac-config-default)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'markdown-mode-hook 'outline-minor-mode)
+(add-hook 'markdown-mode-hook 'visual-line-mode)
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-hook 'yaml-mode-hook 'visual-line-mode)
+
+
+(package-initialize)
+(elpy-enable)
+(setenv "WORKON_HOME" "/usr/local/Caskroom/miniconda/base/envs/")
 
 (setq org-log-done 'time)
-(setq org-todo-keywords '((sequence "TODO" "WAITING" "DONE")))
-(setq org-tag-alist '(("IU" . nil) ("I!U" . nil) ("!IU" . nil) ("!I!U" . nil)))
+(setq org-todo-keywords
+      '((sequence "TODO" "PROGRESS" "|" "DONE")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(epresent yaml-mode visual-fill-column markdown-mode json-mode flycheck elpy auto-complete)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
-(require 'flyspell)
-(flyspell-mode +1)
+(setq-default show-trailing-whitespace t)
+(setq-default indicate-empty-lines t)
+
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/backup/" t)))
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup/")))
+(setq create-lockfiles nil)
+(column-number-mode)
+
