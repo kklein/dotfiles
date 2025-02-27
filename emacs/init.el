00b60e78c05f45e64dd206d322b6d2c3e45be3f4
kklein <kleinkevin.lu@gmail.com>
2025-02-27 19:25:55 +0100

    Rework emacs config.

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,5 @@
+;; Basic UI and behavior settings
 (setq mac-command-modifier 'meta)
-
 (setq inhibit-startup-message t)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
@@ -7,92 +7,105 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 (global-auto-revert-mode t)
 (global-display-line-numbers-mode)
-
-;; Dependency management
-(require 'package)
-(require 'use-package)
-
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
-
-(defvar my-packages '(auto-complete
-		      elpy
-		      flycheck
-                      json-mode
-                      markdown-mode
-                      yaml-mode
-		      rst
-		      magit
-		      multiple-cursors
-		      conda
-		      rust-mode
-		      csv-mode
-		      graphviz-dot-mode
-                      ))
-
-(dolist (p my-packages)
-  (unless (package-installed-p p)
-    (package-refresh-contents)
-    (package-install p))
-  (add-to-list 'package-selected-packages p))
-
-(remove-hook 'text-mode-hook #'turn-on-auto-fill)
-(remove-hook 'markdown-mode-hook #'turn-on-auto-fill)
-
-(add-hook 'after-init-hook #'global-flycheck-mode)
-(add-hook 'text-mode-hook 'flyspell-mode)
-(add-hook 'markdown-mode-hook 'outline-minor-mode)
-(add-hook 'markdown-mode-hook 'visual-line-mode)
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-(add-hook 'yaml-mode-hook 'visual-line-mode)
-
-(elpy-enable)
-(setenv "WORKON_HOME" "/Users/kevinklein/micromamba/envs")
-(setq elpy-rpc-virtualenv-path "/Users/kevinklein/micromamba/envs/elpy-rpc/")
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "-i --simple-prompt")
-(setq elpy-eldoc-show-current-function nil)
-
-
-(setq org-log-done 'time)
-(setq org-todo-keywords
-      '((sequence "TODO" "PROGRESS" "|" "DONE" "ABORTED")))
-
-
-(setq-default show-trailing-whitespace t)
-(setq-default indicate-empty-lines t)
-
-(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/backup/" t)))
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup/")))
-(setq create-lockfiles nil)
 (column-number-mode)
 
-(setq-default flycheck-disabled-checkers '(python-pylint))
+;; Package repositories setup
+(require 'package)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(elpy-modules
-   '(elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-sane-defaults))
- '(package-selected-packages
-   '(impatient-mode magit rst yaml-mode visual-fill-column markdown-mode json-mode flycheck elpy auto-complete))
- '(warning-suppress-log-types '(((python python-shell-completion-native-turn-on-maybe)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Setup use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)  ;; Always ensure packages are installed
 
-(global-set-key (kbd "C-c m c") 'mc/edit-lines)
-
-(use-package graphviz-dot-mode
-  :ensure t
+;; General editor settings
+(use-package emacs
+  :ensure nil  ;; 'emacs' is a built-in package
   :config
-  (setq graphviz-dot-indent-width 4))
+  (setq-default show-trailing-whitespace t)
+  (setq-default indicate-empty-lines t)
+  (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/backup/" t)))
+  (setq backup-directory-alist '(("." . "~/.emacs.d/backup/")))
+  (setq create-lockfiles nil)
+  (remove-hook 'text-mode-hook #'turn-on-auto-fill))
 
-(setq markdown-command '("pandoc" "--from=markdown" "--to=html5"))
+;; Text modes and spell checking
+(use-package flyspell
+  :hook (text-mode . flyspell-mode))
 
+;; Auto-complete
+(use-package auto-complete
+  :config
+  (ac-config-default))
+
+;; Python development with elpy
+(use-package elpy
+  :init
+  (elpy-enable)
+  :config
+  (setenv "WORKON_HOME" "/Users/kevinklein/micromamba/envs")
+  (setq elpy-rpc-virtualenv-path "/Users/kevinklein/micromamba/envs/elpy-rpc/")
+  (setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args "-i")
+  (setq elpy-eldoc-show-current-function nil)
+  :custom
+  (elpy-modules '(elpy-module-company
+                  elpy-module-eldoc
+                  elpy-module-pyvenv
+                  elpy-module-highlight-indentation
+                  elpy-module-yasnippet
+                  elpy-module-sane-defaults)))
+
+;; Conda support
+(use-package conda)
+
+;; Syntax checking
+(use-package flycheck
+  :init
+  (global-flycheck-mode)
+  :config
+  (setq-default flycheck-disabled-checkers '(python-pylint)))
+
+;; Markdown mode
+(use-package markdown-mode
+  :hook ((markdown-mode . outline-minor-mode)
+         (markdown-mode . visual-line-mode)))
+
+;; YAML mode
+(use-package yaml-mode
+  :mode "\\.yml\\'"
+  :hook (yaml-mode . visual-line-mode))
+
+;; JSON mode
+(use-package json-mode)
+
+;; RST mode
+(use-package rst)
+
+;; Git interface
+(use-package magit)
+
+;; Multiple cursors
+(use-package multiple-cursors
+  :bind (("C-c m c" . mc/edit-lines)))
+
+;; Rust mode
+(use-package rust-mode)
+
+;; Org mode configuration
+(use-package org
+  :ensure nil  ;; org is built-in
+  :config
+  (setq org-log-done 'time)
+  (setq org-todo-keywords
+        '((sequence "TODO" "PROGRESS" "|" "DONE" "ABORTED"))))
+
+;; Web preview mode
+(use-package impatient-mode)
+
+(setq warning-suppress-log-types '(((python python-shell-completion-native-turn-on-maybe))))
