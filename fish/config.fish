6466083467f5312d59d070633c8c6d9b305c4d1b
kklein <kleinkevin.lu@gmail.com>
2026-01-06 10:00:54 +0100

    Update fish for pixi

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,3 @@
 /opt/homebrew/bin/brew shellenv | source
 
-
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'micromamba shell init' !!
-set -gx MAMBA_EXE "/opt/homebrew/bin/micromamba"
-set -gx MAMBA_ROOT_PREFIX "/Users/kevin.klein/.local/share/mamba"
-$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
-# <<< mamba initialize <<<
+pixi completion --shell fish | sourcex
